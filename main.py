27e169099d15b59b75ff2cb44f42da9c44d11bfc
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-27 01:35:33 -0400

    add plano.json

--- main.py
+++ main.py
@@ -11,42 +11,70 @@ if __name__ == '__main__':
     st.divider()
 
     df = pd.read_excel('biblia.ods', engine='odf')
-
-    df = df.drop(['pagina_inicial'], axis=1)
-
-    df['lido'] = [True if i == 1 else False for i in df['lido']]
-
-    row = st.columns(3)
-
-    livro = df.groupby('lido')['livro'].count().reset_index()
-    livro['lido'] = np.where(livro['lido'], 'Lido', 'Não lido')
-
-    fig = px.pie(
-        livro, values='livro', names='lido', title='Percentual lido por livros'
+    df = df.drop(['pagina_inicial', 'quantidade_paginas'], axis=1)
+    df['status'] = np.where(
+        df['capitulos'] == df['capitulos_lidos'], 'Lidos', 'Não Lidos'
     )
 
-    row[0].plotly_chart(fig, True)
+    df = st.data_editor(df, width=2000)
 
-    qtd_paginas = df.groupby('lido')['quantidade_paginas'].sum().reset_index()
-    qtd_paginas['lido'] = np.where(qtd_paginas['lido'], 'Lido', 'Não lido')
+    df2 = pd.read_json('plano.json', convert_axes=False)
 
-    fig = px.pie(
-        qtd_paginas,
-        values='quantidade_paginas',
-        names='lido',
-        title='Percentual lido por páginas',
-    )
+    st.dataframe(df2)
 
-    row[1].plotly_chart(fig, True)
+    row = st.columns(2)
 
-    capitulos = df.groupby('lido')['capitulos'].sum().reset_index()
-    capitulos['lido'] = np.where(capitulos['lido'], 'Lido', 'Não lido')
+    options = ['Por livro', 'Por capítulo']
 
-    fig = px.pie(
-        capitulos,
-        values='capitulos',
-        names='lido',
-        title='Percentual lido por capítulos',
-    )
+    selected = row[1].selectbox('Progresso', options)
 
-    row[2].plotly_chart(fig, True)
+    if selected == options[0]:
+        livro = df.groupby('status')['livro'].count().reset_index()
+        fig = px.pie(
+            livro,
+            values='livro',
+            names='status',
+            title='Percentual lido por livros',
+        )
+        row[1].plotly_chart(fig, True)
+
+    if selected == options[1]:
+        capitulos = df.sum()
+        fig = px.pie(
+            capitulos,
+            values=[capitulos['capitulos'], capitulos['capitulos_lidos']],
+            names=['Não Lidos', 'Lidos'],
+            title='Percentual lido por capítulos',
+        )
+        row[1].plotly_chart(fig, True)
+
+    # st.latex('TESTE')
+    # st.divider()
+
+    # st.toast('teste')
+
+    # tab = st.tabs(['# teste', '# teste'])
+
+    # # st.snow()
+
+    # st.header('teste')
+    # st.subheader('teste')
+
+    # # st.switch_page()
+
+    # tab[0].error('teste 1')
+    # tab[1].error('teste 2')
+
+    # qtd_paginas = df.groupby('lido')['quantidade_paginas'].sum().reset_index()
+    # qtd_paginas['lido'] = np.where(qtd_paginas['lido'], 'Lido', 'Não lido')
+
+    # fig = px.pie(
+    #     qtd_paginas,
+    #     values='quantidade_paginas',
+    #     names='lido',
+    #     title='Percentual lido por páginas',
+    # )
+
+    # row[1].plotly_chart(fig, True)
+
+    # st.dataframe(capitulos)
