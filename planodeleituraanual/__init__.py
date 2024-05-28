ba34edbb12c954fae5d22d49a43a227cdfb9f633
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-28 02:35:58 -0400

    update

--- planodeleituraanual/__init__.py
+++ planodeleituraanual/__init__.py
@@ -31,19 +31,28 @@ def create_app():
         data = row[0].date_input('Data Específica:', format='DD/MM/YYYY')
 
         leituras = db.get_bread_daily(data)
+        if not leituras:
+            if data.strftime('%m-%d') == '02-29':
+                row[0].error(
+                    'Esse ano é bissexto, o dia 29/02 não está no plano.'
+                )
+            else:
+                row[0].error('Ops! Algo deu errado!')
+        else:
+            row[0].divider()
 
-        row[0].divider()
-
-        for item in leituras:
-            row_leituras = row[0].columns([1.5, 0.5])
-            row_leituras[0].write('##### {}'.format(item.replace('_', ' ')))
-            row_leituras[1].toggle(
-                'Marque como Lido',
-                value=db.is_chapter_read(item),
-                key=item,
-                on_change=db.set_chapter_read,
-                args=[item],
-            )
+            for item in leituras:
+                row_leituras = row[0].columns([1.5, 0.5])
+                row_leituras[0].write(
+                    '##### {}'.format(item.replace('_', ' '))
+                )
+                row_leituras[1].toggle(
+                    'Marque como Lido',
+                    value=db.is_chapter_read(item),
+                    key=item,
+                    on_change=db.set_chapter_read,
+                    args=[item],
+                )
 
         row[1].write('## Progresso')
 
