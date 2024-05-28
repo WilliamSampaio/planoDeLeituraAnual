b14bcfbc7583bc158a406dc13dadd41d8feb2c45
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-27 22:36:38 -0400

    update

--- database.py
+++ database.py
@@ -46,3 +46,32 @@ def get_bread_daily(date: dt.date = dt.date.today()) -> dict:
             if bread is not None:
                 return bread['leituras']
     return None
+
+
+def count_chapters_read(book: str) -> int:
+    usuario = get_usuario()
+    if usuario is None:
+        return None
+    return len(
+        [x.split('_')[1] for x in usuario['lidos'] if x.split('_')[0] == book]
+    )
+
+
+def is_chapter_read(chapter: str):
+    usuario = get_usuario()
+    if usuario is None:
+        return False
+    return True if chapter in usuario['lidos'] else False
+
+
+def set_chapter_read(chapter: str):
+    usuario = get_usuario()
+    if usuario is None:
+        return None
+    lidos = usuario['lidos']
+    if chapter in lidos:
+        lidos.remove(chapter)
+    else:
+        lidos.append(chapter)
+    usuario['lidos'] = lidos
+    return get_usuario_tbl().update(usuario, doc_ids=[1])
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 from dotenv import dotenv_values
 
 import database as db
 from create_plan import create_plan
+
+
+@st.cache_data
+def load_biblia_df():
+    return pd.read_json('biblia.df.json')
+
 
 if __name__ == '__main__':
 
@@ -19,7 +24,6 @@ if __name__ == '__main__':
         usuario = db.get_usuario()
 
         st.write('# {}'.format(config['APP_NAME']))
-        st.divider()
 
         container = st.container()
 
@@ -36,13 +40,45 @@ if __name__ == '__main__':
         row_leituras = row[0].columns([1.5, 0.5])
         for item in leituras:
             row_leituras[0].write(item.replace('_', ' '))
-            row_leituras[1].toggle('Marque como Lido', value=False, key=item)
+            row_leituras[1].toggle(
+                'Marque como Lido',
+                value=db.is_chapter_read(item),
+                key=item,
+                on_change=db.set_chapter_read,
+                args=[item],
+            )
 
-        df = pd.read_excel('biblia.ods', engine='odf')
-        df = df.drop(['pagina_inicial', 'quantidade_paginas'], axis=1)
-        df['status'] = np.where(
-            df['capitulos'] == df['capitulos_lidos'], 'Lidos', 'Não Lidos'
-        )
+        row[1].write('## Progresso')
+
+        options = ['Por livro', 'Por capítulo']
+
+        selected = row[1].selectbox('Acompanhar Por:', options)
+
+        # if selected == options[0]:
+        #     livro = df.groupby('status')['livro'].count().reset_index()
+        #     fig = px.pie(
+        #         livro,
+        #         values='livro',
+        #         names='status',
+        #         title='Percentual lido por livros',
+        #     )
+        #     row[1].plotly_chart(fig, True)
+
+        # if selected == options[1]:
+        #     capitulos = df.sum()
+        #     fig = px.pie(
+        #         capitulos,
+        #         values=[capitulos['capitulos'], capitulos['capitulos_lidos']],
+        #         names=['Não Lidos', 'Lidos'],
+        #         title='Percentual lido por capítulos',
+        #     )
+        #     row[1].plotly_chart(fig, True)
+
+        # df = pd.read_excel('biblia.ods', engine='odf')
+        # df = df.drop(['pagina_inicial', 'quantidade_paginas'], axis=1)
+        # df['status'] = np.where(
+        #     df['capitulos'] == df['capitulos_lidos'], 'Lidos', 'Não Lidos'
+        # )
 
         # st.write(tbl.get(doc_id=99))
 
