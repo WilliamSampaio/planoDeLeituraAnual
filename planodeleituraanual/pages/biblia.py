5101fc1581de4c0f4f01e8944375581849840891
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-28 15:30:44 -0400

    update

--- planodeleituraanual/pages/biblia.py
+++ planodeleituraanual/pages/biblia.py
@@ -1,20 +1,47 @@
+import streamlit as st
 from streamlit.delta_generator import DeltaGenerator
 
-from planodeleituraanual.acf import get_books, get_chapter_count
+from planodeleituraanual import acf
 
 
 def chunks(lst, n):
     for i in range(0, len(lst), n):
-        yield lst[i : i + n]
+        yield lst[i:i + n]
 
 
 def index(tab: DeltaGenerator):
 
-    livro = tab.selectbox('Livro:', get_books())
+    if 'capitulo' not in st.session_state:
+        st.session_state.capitulo = 1
+
+    def set_capitulo(capitulo):
+        st.session_state.capitulo = capitulo
+
+    livro = tab.selectbox('Livro:', acf.get_books())
 
     tab.write('#### Capítulos')
 
-    chapter_list = range(1, get_chapter_count(livro) + 1)
+    chapter_list = range(1, acf.get_chapter_count(livro) + 1)
+
+    for sub_list in chunks(chapter_list, 10):
+        btn_chapters = tab.columns(10)
+        for i in range(len(sub_list)):
+            btn_chapters[i].button(
+                '**{}**'.format(str(sub_list[i])),
+                '{}_{}'.format(livro, sub_list[i]),
+                on_click=set_capitulo,
+                args=[sub_list[i]],
+            )
+    tab.divider()
+    conteudo = acf.get_chapter(
+        acf.get_book_id(livro), st.session_state.capitulo
+    )
+    if conteudo is not None:
+        for verse in conteudo['verses']:
+            for key, value in verse.items():
+                tab.markdown(
+                    '{}. {}'.format(key, value), unsafe_allow_html=True
+                )
 
     # print(chunks(chapter_list, 10))
 
